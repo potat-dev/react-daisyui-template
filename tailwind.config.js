aa579a1f124b07bf289bfa04bff378302a39f30b
Den <denis100703@gmail.com>
2022-08-14 23:19:16 +0300

    Install & setup Tailwind and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["night", "winter"],
+  },
+};
